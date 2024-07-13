dfe660811c65e060131b626be926fbfeae534604
yhlooo <yhlooo.x@outlook.com>
2024-07-13 16:36:00 +0800

    feat: support for using cache when completing `get` and `describe` subcommands

--- pkg/commands/get.go
+++ pkg/commands/get.go
@@ -11,7 +11,10 @@ import (
 
 // NewGetCommand 创建 get 子命令
 func NewGetCommand(clientGetter genericclioptions.RESTClientGetter) *cobra.Command {
-	f, cmd := cmdutil.NewKubectlCommandWithInternalProxy(clientGetter, "cache", cmdget.NewCmdGet)
-	cmd.ValidArgsFunction = utilcomp.ResourceTypeAndNameCompletionFunc(f)
-	return cmd
+	return cmdutil.NewKubectlCommandWithInternalProxy(
+		clientGetter,
+		"cache",
+		cmdget.NewCmdGet,
+		utilcomp.ResourceTypeAndNameCompletionFunc,
+	)
 }
